e2a5100d0ff750b3b82543f6a1166e1bf0c957f3
Marek <marek.kregiel@storm-technologies.com>
2022-03-09 16:26:00 +0000

    chore: configure api service for the headlines

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { NewsService } from './api/newsService';
 import './index.css';
 
 
 const App = () => {
-
+  const getTopHeadlines = async () => {
+    const res = await NewsService.getTopHeadlines({ country: 'us' })
+    console.log('res: ', res);
+  }
+  useEffect(() => {
+    getTopHeadlines();
+  }, [])
   return (
     <div className="App">
       123
---
+++ src/api/newsService.js
@@ -0,0 +1,5 @@
+const apiKey = process.env.API_KEY || '17dd3ac3633a465487f2d1662bd8490f';
+
+export class NewsService {
+  static getTopHeadlines = async ({ country, pageSize = 5 }) => fetch(`https://newsapi.org/v2/top-headlines?country=${country}&pageSize=${pageSize}&apiKey=${apiKey}`).then(res => res.json());
+}
