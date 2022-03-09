461fefd0b8be5043be08e500e11f0d64e44bab2b
Marek <marek.kregiel@storm-technologies.com>
2022-03-09 15:41:05 +0000

    chore: initial repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+import './index.css';
+
+
+const App = () => {
+
+  return (
+    <div className="App">
+      123
+    </div>
+  );
+}
+
+export default App;
