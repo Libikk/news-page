0b72529d2c73f66c00373004af562dd7c495260a
Marek <marek.kregiel@storm-technologies.com>
2022-03-10 10:32:49 +0000

    chore: filters mobile version

--- src/components/NewsCategoryFilters.js
+++ src/components/NewsCategoryFilters.js
@@ -9,6 +9,18 @@ const NewsCategoryFilters = () => {
     ]
     return (
         <div>
+            <div className="sm:hidden">
+                <select
+                id="tabs"
+                name="tabs"
+                className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                defaultValue={'us'}
+                >
+                {countries.map((tab) => (
+                    <option key={tab.label}>{tab.label}</option>
+                ))}
+                </select>
+            </div>
             <div className="hidden sm:block">
                 <div className="border-b border-gray-200">
                 <nav className="-mb-px flex space-x-8" aria-label="Tabs">
