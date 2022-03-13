0ea8e5eb0d56ee6d15bb50c7423db9ace439ba6a
Libikk <libikpl@gmail.com>
2022-03-13 12:03:06 +0000

    style: articles layout + first headline classes

--- src/components/TopHeadlines.js
+++ src/components/TopHeadlines.js
@@ -4,11 +4,17 @@ const TopHeadlines = ({ topHeadlines }) => {
     if (!topHeadlines) return <div>Loading...</div>
 
     const headlinesWithImages = topHeadlines.filter(({ urlToImage }) => urlToImage);
-    const [firstHeadline, ...rest] = headlinesWithImages
+    const isFirstHeadline = (index) => index === 0;
 
-    return <div>
-        <Article article={firstHeadline} />
-    </div>
+    return (
+        <div className="grid grid-cols-4 gap-8 mt-10">
+            {headlinesWithImages.map((headline, i) =>
+                <div className={`${isFirstHeadline(i) && 'col-start-1 col-end-5 row-start-1 row-end-4'}`}>
+                    <Article article={headline}  />
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default TopHeadlines;
