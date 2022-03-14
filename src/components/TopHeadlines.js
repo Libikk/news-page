3b41b3acfea3357a587332fcd2508ccaa173e6ad
Libikk <libikpl@gmail.com>
2022-03-14 09:07:29 +0000

    style: change layour of the articles grid + responsive layout

--- src/components/TopHeadlines.js
+++ src/components/TopHeadlines.js
@@ -3,16 +3,15 @@ import Article from "./Article";
 const TopHeadlines = ({ topHeadlines }) => {
     if (!topHeadlines) return <div>Loading...</div>
 
-    const headlinesWithImages = topHeadlines.filter(({ urlToImage }) => urlToImage);
-    const isFirstHeadline = (index) => index === 0;
+    const headlinesWithImages = topHeadlines.filter(({ urlToImage }) => urlToImage).slice(0, 5);
+    const [firstHeadline, ...restHeadlines] = headlinesWithImages;
 
     return (
-        <div className="grid grid-cols-4 gap-8 mt-10">
-            {headlinesWithImages.map((headline, i) =>
-                <div className={`${isFirstHeadline(i) && 'col-start-1 col-end-5 row-start-1 row-end-4'}`}>
-                    <Article article={headline}  />
-                </div>
-            )}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 my-10">
+            <div className="sm:col-span-2 md:col-span-4">
+                <Article article={firstHeadline} isBrakingNews />
+            </div>
+            {restHeadlines.map((headline) => <Article key={headline.title} article={headline}  />)}
         </div>
     )
 }
